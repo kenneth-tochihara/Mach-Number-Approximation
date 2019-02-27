2ffce3764a94c1b8ad934e22e96481aeaea0c61d
Kenneth Tochihara <ktt3@illinois.edu>
2019-02-27 01:14:28 -0600

    Create machNum_approx_UI37.py

---
+++ UI Examples/machNum_approx_UI37.py
@@ -0,0 +1,34 @@
+#Import some stuff
+from scipy import interpolate
+import numpy as np
+
+#Constants and arrays
+machNums = np.arange(0.0, 1.601, 0.001)
+pRatio = []
+gam = 1.4
+
+#Calculates the pressure ratios
+for m1 in machNums:
+    if m1 < 1:
+        #Find p01/p1
+        pRatio.append(((1 + ((gam - 1)/2)*(m1 ** 2))**(gam/(gam - 1))))
+    else:
+        #Find p02/p1
+        pRatio.append((((gam + 1)**2 * m1**2)/((4 * gam * m1**2) - 2 * (gam - 1)))**(gam /(gam - 1)) * ((1 - gam + (2 * gam * (m1**2)))/(gam + 1)))
+
+#Interpolation
+f = interpolate.interp1d(pRatio, machNums)
+
+#Tell user input range
+print(f'Min: {min(pRatio)}\nMax: {max(pRatio)}')
+
+#User Input/Output
+ans = ""
+while ans is "":
+    try:
+        p = float(input("Pressure Ratio: "))
+        print(f"Mach Number: {f(p)}")
+    except:
+        print("Invalid input.") #Takes into account the fact that input could be invalid.
+        pass
+    ans = input("Again? (Enter to continue)")
